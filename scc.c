784e06c74e66fddd554dbd3316f8296ae9643be2
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-11-22 23:49:24 -0300

    working

--- scc.c
+++ scc.c
@@ -7,9 +7,8 @@ typedef struct {
   int u;
   int v;
 } Edge;
-typedef struct graph *Graph;
-typedef struct node *link;
 
+typedef struct node *link;
 struct node {
   Vertex w;
   link next;
@@ -19,7 +18,19 @@ struct graph {
   int V;
   int E;
   link *adj;
+  int *discovery;
+  int *low;
+  int *onStack;
+  int *component;
+  int time;
 };
+
+typedef struct graph *Graph;
+
+typedef struct {
+  int *vertices;
+  int count;
+} StronglyConnectedComponent;
 
 void *Malloc(size_t size) {
   void *ptr = malloc(size);
@@ -38,7 +49,6 @@ link list_insert(link head, Vertex w) {
 link list_remove(link head, Vertex w) {
   if (head == NULL)
     return NULL;
-
   if (head->w == w) {
     link p = head->next;
     free(head);
@@ -59,16 +69,20 @@ link list_destroy(link head) {
 
 Graph graph(int V) {
   assert(V > 0);
-
-  // http://color-faq.com/aryptr/fn33.html
   Graph G = Malloc(sizeof(*G));
-
   G->V = V;
   G->E = 0;
   G->adj = Malloc(V * sizeof(link));
+  G->discovery = Malloc(V * sizeof(int));
+  G->low = Malloc(V * sizeof(int));
+  G->onStack = Malloc(V * sizeof(int));
+  G->component = Malloc(V * sizeof(int));
+  G->time = 0;
 
-  for (Vertex u = 0; u < V; u++)
+  for (Vertex u = 0; u < V; u++) {
     G->adj[u] = NULL;
+    G->onStack[u] = 0;
+  }
 
   return G;
 }
@@ -77,9 +91,7 @@ void graph_insert_edge(Graph G, Edge e) {
   assert(G);
   assert(e.u >= 0 && e.u < G->V);
   assert(e.v >= 0 && e.v < G->V);
-
   G->adj[e.u] = list_insert(G->adj[e.u], e.v);
-  G->adj[e.v] = list_insert(G->adj[e.v], e.u);
   G->E += 1;
 }
 
@@ -87,8 +99,100 @@ Edge edge(int u, int v) {
   Edge e = {u, v};
   return e;
 }
-int main(int argc, char const *argv[]) {
 
+StronglyConnectedComponent *create_scc(int size) {
+  StronglyConnectedComponent *scc = Malloc(sizeof(StronglyConnectedComponent));
+  scc->vertices = Malloc(size * sizeof(int));
+  scc->count = 0;
+  return scc;
+}
+
+void destroy_scc(StronglyConnectedComponent *scc) {
+  free(scc->vertices);
+  free(scc);
+}
+
+int compare_integers(const void *a, const void *b) {
+  return (*(int *)a - *(int *)b);
+}
+
+// me ajuda a printar as regioes altamente conectadas em ordem crescente
+void print_scc(StronglyConnectedComponent *scc) {
+  qsort(scc->vertices, scc->count, sizeof(int), compare_integers);
+  for (int i = 0; i < scc->count; i++) {
+    printf("%d ", scc->vertices[i]);
+  }
+  printf("\n");
+}
+
+void tarjan_dfs(Graph G, Vertex v, int *stack, int *top,
+                StronglyConnectedComponent **sccList, int *sccCount) {
+  G->discovery[v] = G->low[v] = ++G->time;
+  stack[++(*top)] = v;
+  G->onStack[v] = 1;
+
+  for (link p = G->adj[v]; p; p = p->next) {
+    Vertex w = p->w;
+    if (G->discovery[w] == 0) {
+      tarjan_dfs(G, w, stack, top, sccList, sccCount);
+      G->low[v] = G->low[v] < G->low[w] ? G->low[v] : G->low[w];
+    } else if (G->onStack[w]) {
+      G->low[v] = G->low[v] < G->discovery[w] ? G->low[v] : G->discovery[w];
+    }
+  }
+
+  if (G->discovery[v] == G->low[v]) {
+    StronglyConnectedComponent *scc = create_scc(G->V);
+    (*sccCount)++;
+    while (*top >= 0 && stack[*top] != v) {
+      Vertex w = stack[(*top)--];
+      G->onStack[w] = 0;
+      G->component[w] = G->time;
+      scc->vertices[scc->count++] = w;
+    }
+    Vertex w = stack[(*top)--];
+    G->onStack[w] = 0;
+    G->component[w] = G->time;
+    scc->vertices[scc->count++] = w;
+    sccList[(*sccCount) - 1] = scc;
+  }
+}
+
+void tarjan(Graph G) {
+  int *stack = Malloc(G->V * sizeof(int));
+  int top = -1;
+
+  StronglyConnectedComponent **sccList =
+      Malloc(G->V * sizeof(StronglyConnectedComponent *));
+  int sccCount = 0;
+
+  for (Vertex v = 0; v < G->V; v++) {
+    if (G->discovery[v] == 0) {
+      tarjan_dfs(G, v, stack, &top, sccList, &sccCount);
+    }
+  }
+
+  free(stack);
+
+  if (sccCount == 1) {
+    printf("Ha 1 regiao altamente conectada:\n");
+    for (int i = 0; i < sccCount; i++) {
+      print_scc(sccList[i]);
+      destroy_scc(sccList[i]);
+    }
+    return;
+  }
+
+  printf("Ha %d regioes altamente conectadas:\n", sccCount);
+  for (int i = 0; i < sccCount; i++) {
+    print_scc(sccList[i]);
+    destroy_scc(sccList[i]);
+  }
+
+  free(sccList);
+}
+
+int main() {
   int V, E;
   scanf("%d %d", &V, &E);
 
@@ -99,6 +203,8 @@ int main(int argc, char const *argv[]) {
     scanf("%d %d", &u, &v);
     graph_insert_edge(G, edge(u, v));
   }
+
+  tarjan(G);
 
   return 0;
 }
